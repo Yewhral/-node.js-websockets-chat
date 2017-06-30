16329fb1cacccf9fd25b7dc11341b9fa96771168
Yewhral <zabrzyjewski@gmail.com>
2017-06-30 14:33:09 +0200

    Added styling, input protection and timestamps

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # -node.js-websockets-chat
+
+I wrote a simple chat to get better understanding of websockets (using sockets.io).
+This web app also uses other Node.js tool - Express, JS ECMA6, and of course some basic CSS and HTML.
--- public/chat.js
+++ public/chat.js
@@ -6,13 +6,22 @@ const sendButton = document.getElementById('send');
 const output = document.getElementById('output');
 const feedback = document.getElementById('feedback');
 
+
+const sendMessage = () => {
+    if (message.value.length > 0 && username.value.length > 0) {
+        socket.emit('chat', {
+            message: message.value,
+            name: username.value
+        });
+        message.value = '';
+    } else {
+        alert('Don\'t send messages without content or your name!');
+    }
+};
+
 // Emitters
 sendButton.addEventListener('click', () => {
-    socket.emit('chat', {
-        message:message.value,
-        name:username.value
-    });
-    message.value = '';
+  sendMessage();
 });
 
 message.addEventListener('keypress', () => {
@@ -22,10 +31,30 @@ message.addEventListener('keypress', () => {
 });
 
 
+const getTime = () => {
+  let timeNow = new Date();
+  let hh = timeNow.getHours();
+  let mm = timeNow.getMinutes()+1;
+  let ss = timeNow.getSeconds();
+
+    if(hh < 10) {
+        hh = '0' + hh
+    }
+    if(mm < 10) {
+        mm = '0' + mm
+    }
+    if(ss < 10) {
+        ss = '0' + ss
+    }
+    timeNow = '[' + hh + ':' + mm + ':' + ss + ']';
+    return timeNow;
+
+};
+
 // Listeners
 socket.on('chat', (data) => {
     feedback.innerHTML = "";
-    output.innerHTML += '<p><strong>' + data.name + ': </strong>' + data.message + '</p>';
+    output.innerHTML += '<p> '+ getTime() + ' <strong>' + data.name + ': </strong>' + data.message + '</p>';
 });
 
 socket.on('typing', (data) => {
